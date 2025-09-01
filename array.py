83cb6d2c848e0cb854f6588d49a76ac667459b37
abhishekkumar956821 <abhishekkumar956821@gmail.com>
2025-09-01 10:48:46 +0530

    first commit

--- array.py
+++ array.py
@@ -1,8 +1,29 @@
-# mark1 = 85
-# mark2 = 90
-# mark3 = 100
-# marks4 = 33
+# # mark1 = 85
+# # mark2 = 90
+# # mark3 = 100
+# # marks4 = 33
 
-# With the help of array 
+# # With the help of array 
 
-# marks = [90,85,55,44,66]
+# # marks = [90,85,55,44,66]
+
+# different types of array 
+
+# like 
+
+# one-d array
+
+# [1,2,3]
+
+# 2d array 
+
+# rows & columns 
+
+# [[1,2,3,4,],
+#  [1,2,3,4],
+#  [1,2,3,4]
+
+#  3d array 
+#  stack of multiple of array 
+#  ]
+
